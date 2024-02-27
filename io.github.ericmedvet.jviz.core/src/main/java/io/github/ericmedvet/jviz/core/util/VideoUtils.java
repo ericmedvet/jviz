0ff4822dac1cd0c48d5598e98608209e3b3ea540
Eric Medvet <eric.medvet@gmail.com>
2024-02-27 10:59:07 +0100

    Reformatting.

--- io.github.ericmedvet.jviz.core/src/main/java/io/github/ericmedvet/jviz/core/util/VideoUtils.java
+++ io.github.ericmedvet.jviz.core/src/main/java/io/github/ericmedvet/jviz/core/util/VideoUtils.java
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * jviz-core
+ * %%
+ * Copyright (C) 2024 Eric Medvet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 /*
  * Copyright 2024 eric
  *
@@ -16,15 +35,6 @@
 
 package io.github.ericmedvet.jviz.core.util;
 
-import org.jcodec.api.SequenceEncoder;
-import org.jcodec.common.Format;
-import org.jcodec.common.io.NIOUtils;
-import org.jcodec.common.io.SeekableByteChannel;
-import org.jcodec.common.model.ColorSpace;
-import org.jcodec.common.model.Picture;
-import org.jcodec.common.model.Rational;
-
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -36,6 +46,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import org.jcodec.api.SequenceEncoder;
+import org.jcodec.common.Format;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.io.SeekableByteChannel;
+import org.jcodec.common.model.ColorSpace;
+import org.jcodec.common.model.Picture;
+import org.jcodec.common.model.Rational;
 
 public class VideoUtils {
 
