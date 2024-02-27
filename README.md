2bd85ae69fd562d1661882cb014b03effe8f5669
Eric Medvet <eric.medvet@gmail.com>
2024-02-27 10:58:27 +0100

    Dev version update.

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ Add (at least) this to your `pom.xml`:
 <dependency>
     <groupId>io.github.ericmedvet</groupId>
     <artifactId>jviz.core</artifactId>
-    <version>0.1.0</version>
+    <version>0.1.1-SNAPSHOT</version>
 </dependency>
 ```
--- io.github.ericmedvet.jviz.core/src/main/java/io/github/ericmedvet/jviz/core/plot/CsvPlotter.java
+++ io.github.ericmedvet.jviz.core/src/main/java/io/github/ericmedvet/jviz/core/plot/CsvPlotter.java
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * jviz-core
+ * %%
+ * Copyright (C) 2024 Eric Medvet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 /*
  * Copyright 2024 eric
  *
@@ -20,9 +39,6 @@ import io.github.ericmedvet.jnb.datastructure.Grid;
 import io.github.ericmedvet.jnb.datastructure.HashMapTable;
 import io.github.ericmedvet.jnb.datastructure.Table;
 import io.github.ericmedvet.jviz.core.util.Misc;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -30,6 +46,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 public class CsvPlotter implements Plotter<List<File>> {
 
